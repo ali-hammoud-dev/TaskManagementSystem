e82902119a91680f31ac98d624785c1044582fb1
Ali Hammoud <ali.hammoud98@hotmail.com>
2023-08-20 22:08:43 +0300

    test

--- TaskManagementSystem.Business/Managers/TaskManager.cs
+++ TaskManagementSystem.Business/Managers/TaskManager.cs
@@ -23,7 +23,7 @@ public class TaskManager : BaseManager<TaskModel>, ITaskManager
 
     public TaskManager(ITaskRepository repository,
         IMapper mapper, ILoggerService loggerService,
-        IHttpContextAccessor contextAccessor) : base(repository, mapper, loggerService) => _contextAccessor = contextAccessor;
+        IHttpContextAccessor contextAccessor) : base(repository, mapper, loggerService) => _contextAccessor = contextAccessor; 
 
 
     public async Task<TaskDto?> GetById(int id)
